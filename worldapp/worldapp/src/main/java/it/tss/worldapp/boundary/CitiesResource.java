3792d4f6147582a301b50a6076ef2893b0a79bdf
Rickygre <riccardino7@gmail.com>
2022-04-14 16:24:10 +0200

    14-04-22

--- worldapp/worldapp/src/main/java/it/tss/worldapp/boundary/CitiesResource.java
+++ worldapp/worldapp/src/main/java/it/tss/worldapp/boundary/CitiesResource.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package it.tss.worldapp.boundary;
 
 import it.tss.worldapp.entity.City;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
