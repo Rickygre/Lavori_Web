2bf517fdc042c4456aad86728ac28f2ecd1dea60
Rickygre <riccardino7@gmail.com>
2022-04-28 16:42:43 +0200

    28-04-2022

--- segnalibro-front/public_html/JS/jsfile.js
+++ segnalibro-front/public_html/JS/jsfile.js
@@ -1,3 +1,4 @@
+
 function getBookmarks() {  //funzione carica il file jsonArray di /bookmarks
 
     let path = "http://localhost:8080/segnalibro/resources/bookmarks";
@@ -22,48 +23,72 @@ function getBookmarks() {  //funzione carica il file jsonArray di /bookmarks
             });
 }
 
+function init() {
+    let usr = sessionStorage.getItem("myusr");
+    if (usr)
+    {
+        document.getElementById("loggeduser").innerHTML = usr;
+    } else //non loggato  
+    {
+        document.getElementById("loggeduser").innerHTML = " nessun utente non loggato";
+    }
+}
 
-var globaljwt ="";                //oggetto json che dentro ci sarà jwt   //var variabile globale
 
-function login() {
 
-    let url = "http://localhost:8080/segnalibro/resources/utenti/login"; //path delle risorse 
-    let usr = document.querySelector("#usr").value;  //seleziono campo usr
-    let pwd = document.querySelector("#pwd").value; //seleziono campo pwd
-    let postdata = {
-        "usr": usr,
-        "pwd": pwd
-    };
 
-    postdata = JSON.stringify(postdata); //oggetto body diventa da ogg javascript un ogg stringa json
+    function logout() {
+        sessionStorage.removeItem("globaljwt"); //pulisce solo una proprietà
+        sessionStorage.clear(); //pulisce tutte le sessioni di questo dominio
+        let jwt = sessionStorage.getItem("globaljwt");
+        window.location.href = "/segnalibro-front/index.html";
+    }
 
-    //struttura della chiamata:
-    fetch(url,
-            {
-                method: "post",
-                body: postdata,
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-type': 'application/json'
-                }
 
-            })
-            .then(response =>          //come risposta richiamo la parte della risp json e la rispedisco indietro
-                response.json())
-                        .then(jsobj => {      //jsobj è un parametro che uso per tirare su risorse
-                            globaljwt=jsobj.jwt;
-                            console.log(globaljwt);
+
+
+
+    function login() {
+        let url = "http://localhost:8080/segnalibro/resources/utenti/login"; //path delle risorse 
+        let usr = document.querySelector("#usr").value; //seleziono campo usr
+        let pwd = document.querySelector("#pwd").value; //seleziono campo pwd
+        let postdata = {
+            "usr": usr,
+            "pwd": pwd
+        };
+        postdata = JSON.stringify(postdata); //oggetto body diventa da ogg javascript un ogg stringa json
+
+        //struttura della chiamata:
+        fetch(url,
+                {
+                    method: "post",
+                    body: postdata,
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-type': 'application/json'
+                    }
                 })
-                
 
+                .then(response => {            //come risposta richiamo la parte della risp json e la rispedisco indietro
+                    if (response.status == 401) {
+                        alert("username errato!! inserire le credenziali corrette.");
+                    } else
+                        return response.json();
+                })
 
+                .then(jsobj => {              //jsobj è un parametro che uso per tirare su risorse
+                    if (jsobj != undefined || true) {
+                        sessionStorage.setItem("globaljwt", jsobj.jwt);
+                        let decoded = jwt_decode(jsobj.jwt);
+                        sessionStorage.setItem("myid", decoded.sub);
+                        sessionStorage.setItem("myusr", decoded.upn);
+                        document.querySelector("#loggeduser").innerHTML = sessionStorage.getItem("myusr");
 
-
-
-
-
-
-
-
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                    document.querySelector("#loggeduser").innerHTML = "Utente non trovato!";
+                    });
 
 }
